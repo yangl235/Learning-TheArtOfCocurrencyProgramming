de43171a9ce0e958e10e3a4c0a0c3fdbc4daca42
yanglei18 <yanglei18@meituan.com>
2018-05-18 11:36:41 +0800

    死锁实验

---
+++ src/main/java/ch1/DeadLockDemo.java
@@ -0,0 +1,58 @@
+package ch1;
+
+/**
+ * 死锁实验
+ */
+
+public class DeadLockDemo {
+    public static String obj1 = "obj1";
+    public static String obj2 = "obj2";
+
+    public static void main(String[] args) {
+        Thread a = new Thread(new Lock1());
+        Thread b = new Thread(new Lock2());
+        a.start();
+        b.start();
+    }
+}
+
+class Lock1 implements Runnable {
+    @Override
+    public void run() {
+        try {
+            System.out.println("Lock1 running");
+            while (true) {
+                synchronized (DeadLockDemo.obj1) {
+                    System.out.println("Lock1 lock obj1");
+                    Thread.sleep(3000);
+                    //获取obj1后先等一会儿，让Lock2有足够的时间锁住obj2
+                    synchronized (DeadLockDemo.obj2) {
+                        System.out.println("Lock1 lock obj2");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Lock2 implements Runnable {
+    @Override
+    public void run() {
+        try {
+            System.out.println("Lock2 running");
+            while (true) {
+                synchronized (DeadLockDemo.obj2) {
+                    System.out.println("Lock2 lock obj2");
+                    Thread.sleep(3000);
+                    synchronized (DeadLockDemo.obj1) {
+                        System.out.println("Lock2 lock obj1");
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
